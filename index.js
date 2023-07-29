7838f514b01f3d3ce6b1c5153c15569d86443b1d
Misha Kushka <kushka.misha@gmail.com>
2023-07-28 17:00:24 -0700

    Solution idea

--- index.js
+++ index.js
@@ -174,27 +174,98 @@ const decode = s => {
   return options;
 }
 
+/**
+ * 21211
+ * 2 1 2 1 1
+ * 21 2 1 1
+ * 2 12 1 1
+ * 2 1 21 1
+ * 21 21 1
+ * 
+ * 2 1 2 11
+ * 21 2 11
+ * 2 12 11
+ */
+
+// const decodeSmart = s => {
+//   if (s.length === 0) return 0;
+//   if (s.length === 1) return 1;
+
+//   // s.length >= 2
+//   let prev = '';
+//   let curr = '';
+//   let options = 0;
+
+//   for (let i = 1; i < s.length; i++)
+//   {
+//     // if (i !== 1) options -= 1; // penatly for iteration
+
+//     prev = s[i - 1];
+//     curr = s[i];
+
+//     console.log({ prev, curr, options });
+
+//     if (prev == '0')
+//     {
+//       if (i === 1) return 0; // prev - the first number of the string
+//       return 1;
+//     }
+//     if (curr == '0')
+//     {
+//       if (prev == '1' || prev == '2')
+//       {
+//         options += 1; // '{1,2}0' -> '{1,2}0'
+//         continue;
+//       }
+//       return 0; // '{3,9}0'
+//     } else if (prev == '1' || prev == '2')
+//     {
+//       // prev = {1..9}, curr = {1..9}
+//       if (curr !== '7' && curr !== '8' && curr !== '9')
+//       {
+//         options += 2; // '{1,2}{1..6}' -> '{1,2}{1..6}' or '{1,2}_{1..6}'
+//         continue;
+//       } else
+//       {
+//         options += 1; // '{1,2}{7,8,9}' -> '{1,2}_{7,8,9}'
+//         continue;
+//       }
+//     } else
+//     {
+//       // prev = {3..9}, curr = {1..9}
+//       options += 1; // '{3..9}{1..9}' -> '{3..9}_{1..9}'
+//       // continue;
+//     }
+//   }
+
+//   return options;
+// }
+
 
 const testCases = [
+  // {
+  //   input: '12',
+  //   expectedOutput: 2
+  // },
+  // {
+  //   input: '226',
+  //   expectedOutput: 3
+  // },
+  // {
+  //   input: '06',
+  //   expectedOutput: 0
+  // },
+  // {
+  //   input: '1201',
+  //   expectedOutput: 1
+  // },
+  // {
+  //   input: '2125',
+  //   expectedOutput: 5
+  // },
   {
-    input: '12',
-    expectedOutput: 2
-  },
-  {
-    input: '226',
-    expectedOutput: 3
-  },
-  {
-    input: '06',
-    expectedOutput: 0
-  },
-  {
-    input: '1201',
-    expectedOutput: 1
-  },
-  {
-    input: '2125',
-    expectedOutput: 5
+    input: '21211',
+    expectedOutput: 8
   }
 ]
 
